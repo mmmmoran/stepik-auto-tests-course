fbc1b2196dd9d014f8030857610d7148c76cb5d9
Никита Давидович <nikitadavidovich@MacBook-Pro-Nikita.local>
2020-11-25 18:48:29 +0300

    lesson from stepik

---
+++ lesson2.4_task1.py
@@ -0,0 +1,31 @@
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+from selenium import webdriver
+import math
+import time
+
+try:
+    browser = webdriver.Chrome()
+
+    browser.get("http://suninjuly.github.io/explicit_wait2.html")
+
+    # говорим Selenium проверять в течение 5 секунд, пока кнопка не станет кликабельной
+    price = WebDriverWait(browser, 12).until(EC.text_to_be_present_in_element((By.ID, "price"), "100"))
+    button = browser.find_element_by_id("book")
+    button.click()
+
+    def calc(x):
+        return str(math.log(abs(12 * math.sin(int(x)))))
+    x_element = browser.find_element_by_id("input_value")
+    x = x_element.text
+
+    input = browser.find_element_by_id("answer")
+    input.send_keys(calc(int(x)))
+
+    button_submit = browser.find_element_by_id("solve")
+    button_submit.click()
+
+finally:
+    time.sleep(10)
+    browser.quit()
